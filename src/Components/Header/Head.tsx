b26fb5e99f054881c03df04b522246e723f3d7d5
Rijan-Budds <buddsrijan@gmail.com>
2025-07-23 00:01:19 +0545

    almost done with the header part. now i have to find icons, search bar from shadcn and the header part is completed

--- src/Components/Header/Head.tsx
+++ src/Components/Header/Head.tsx
@@ -1,51 +1,51 @@
-import React from 'react';
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Logo from "../../assets/logo.png"
-import Alert from "../../assets/alert.gif"
+import Logo from "../../assets/logo.png";
+import Alert from "../../assets/alert.gif";
 function Head() {
   return (
     <>
-      <div className='w-full'>
-        <div className='bg-[#bb2649] py-2'>
-          <div className='max-w-[1200px] mx-auto px-4'>
-            <p className='text-white text-center font-medium text-sm m-0'>
+      <div className="w-full">
+        <div className="bg-[#bb2649] py-2">
+          <div className="max-w-[1200px] mx-auto px-4">
+            <p className="text-white text-center font-medium text-sm m-0">
               Please be aware of scams.
-              </p>
+            </p>
           </div>
         </div>
 
-        <header>
-          <div>
-            <div>
-              <div>
+        <header className="w-full py-6 font-medium">
+          <div className="max-w-[1200px] mx-auto px-4">
+            <div className="flex items-center">
+              <div className="flex items-center w-1/6">
                 <button>
                   <Link to="/">
-                    <img src={Logo} alt='logo' />
+                    <img src={Logo} alt="logo" className="w-4/5 h-auto"/>
                   </Link>
                 </button>
               </div>
-              <div>
-                <div>
-                        {/* here goes the searchbox thing*/}
-                    <div>
-                      <button>
-                        <p>post</p>
-                      </button>
-                    </div>
-                    <div>
-                      <button>
-                        <p>userprofile</p>
-                      </button>
-                    </div>
+              <div className="w-5/6">
+                <div className="flex items-center">
+                  {/* here goes the searchbox thing*/}
+                  <div className="ml-4">
+                    <button className="">
+                      <p>post</p>
+                    </button>
+                  </div>
+                  <div className="ml-4">
+                    <button className="">
+                      <p>userprofile</p>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </header>
-      <div>
-      <img src={Alert} alt='alert'/>
-      </div>
+        <div>
+          <img src={Alert} alt="alert" />
+        </div>
       </div>
     </>
   );
